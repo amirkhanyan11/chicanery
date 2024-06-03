db9da00dbd32c45550709bbe60e8da4cd633e086
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2024-06-03 15:37:45 +0400

    developing....

--- Survey.py
+++ Survey.py
@@ -1,23 +1,44 @@
 import re
 import time
+import random
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 
 class Respondent:
 
-    def __init__(self, playwright: Playwright):
+    def __init__(self, playwright: Playwright, qst : dict):
         self.__browser = playwright.chromium.launch(headless=False)
         self.__context = self.__browser.new_context()
         self.__page = self.__context.new_page()
+        self.__questions = qst
+
+
+    def respond(self) -> None:
+        
+        answers = []
+        for q in self.__questions:
+            answers.append(random.randint(1, 100) % len(self.__questions[q])) 
+        
+
+        i = 0;
+        for q in self.__questions:
+            self.__page.wait_for_load_state("networkidle")
+            self.__page.get_by_label(q).locator("div").filter(has_text=self.__questions[q][answers[i]]).nth(2).click()     # self.__page.get_by_label(self.__questions[q][answers[i]]).click()
+            time.sleep(1)
+            i += 1
+
+
+    def send(self) -> None:
+        self.__page.get_by_role("button", name="Ուղարկել").click()
 
 
     def run(self) -> None:
 
-        self.__page.goto("https://docs.google.com/forms/d/e/1FAIpQLScfBKueK5ZNaoLxVzqPWlXsVyGyHwUb5CL9j7sRO0nQEQq8ZQ/viewform?usp=send_form")
-        self.__page.goto("https://docs.google.com/forms/d/e/1FAIpQLScfBKueK5ZNaoLxVzqPWlXsVyGyHwUb5CL9j7sRO0nQEQq8ZQ/viewform")
-        self.__page.get_by_label("Maybe").click()
-        self.__page.get_by_role("button", name="Ուղարկել").click()
+        self.__page.goto("https://forms.gle/TeFsohnJdJBnsy5f8")
+
+        self.respond()
+        self.send()
 
         # ---------------------
 
--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import re
 from playwright.sync_api import Playwright, sync_playwright, expect
 from Xareb import Xareb
 from Survey import Respondent
-
+ 
+questions = {
+    "are u gay?" : ("yes", "no", "Maybe"),
+    # "are you sure?" : ("yesssssss", "sssssssno", "issssssdk")
+    "are you sure?" : ("yes", "no", "idk"),
+    "there's no doubt in your mind?" : ("meh", "i would prefer not to answer")
+}
 
 
 for i in range (1, 5, 1):
     with sync_playwright() as playwright:
-        r = Respondent(playwright)
+        r = Respondent(playwright, questions)
         r.run()
 
 # with sync_playwright() as playwright:
